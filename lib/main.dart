91c4df01e7129733464e7839cdd64a7a7223d556
akmalviya03 <abhishakkrmalviya@gmail.com>
2020-04-10 15:16:14 +0530

    Added List Class

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,24 @@
 import 'package:flutter/material.dart';
 import 'splash.dart';
 import 'selectmode.dart';
+
 void main() {
   Function done = () {
     return 1;
   };
   Map<int, Widget> op = {1: LCO()};
   runApp(MaterialApp(
-    home: SafeArea(
-      child: AnimatedSplash(
-        imagePath: 'images/LCO_transparent.png',
-        home: LCO(),
-        customFunction: done,
-        duration: 2500,
-        type: AnimatedSplashType.BackgroundProcess,
-        outputAndHome: op,
+    home: Scaffold(
+      body: SafeArea(
+        child: AnimatedSplash(
+          imagePath: 'images/LCO_transparent.png',
+          home: LCO(),
+          customFunction: done,
+          duration: 2500,
+          type: AnimatedSplashType.BackgroundProcess,
+          outputAndHome: op,
+        ),
       ),
     ),
   ));
 }
-
-
